251dfaec2a2e58beec9b3662b0bed7c4d09a7c3a
Franck Besnard <besn0847@gmail.com>
2019-08-18 10:46:07 +0200

    updated : timer, detection windows, mask, nocat...

--- catson.py
+++ catson.py
@@ -10,7 +10,7 @@ from skimage.measure import compare_ssim
 
 DEBUG = True
 TARGET_DIR= "/data/pictures"
-TIMER_TO = 5
+TIMER_TO = 30
 RNN_URL = "http://192.168.1.209:5000/process"
 REGEX = "Cat: ([a-zA-Z]+) with probability:([0-9.]+)"
 SLACK_URL = "https://slack.com/api/files.upload" 
@@ -35,14 +35,14 @@ def expired():
     for f in files:
         (cat, proba) = process(reference,f)
         if DEBUG : print("Cat is",cat,"(",proba,")")
-        if ( float(proba) > max_proba ) :
+        if ( float(proba) > max_proba ) and (cat != "nocat") :
             max_proba = float(proba)
             max_cat = cat
             max_file = f
     
     if DEBUG : print(max_cat.upper(),"is detected with probability",max_proba,"in file",max_file)
 
-    if max_proba > 0.98 :
+    if max_proba > 0.80 :
         title = max_cat.upper() + " is detected with probability " + str(max_proba) + " in file " + max_file
         requests.post(SLACK_URL,
                             data={'token': SLACK_API_TOKEN,
@@ -64,6 +64,9 @@ def process(ref, img):
     imageR = cv2.imread(join(TARGET_DIR,ref))
     imageD = cv2.imread(join(TARGET_DIR,img))
 
+    subimg = cv2.absdiff(imageD, imageR)
+    subimg = cv2.threshold(cv2.cvtColor(subimg, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_OTSU)[1]
+
     grayR = cv2.cvtColor(imageR, cv2.COLOR_BGR2GRAY)
     grayD = cv2.cvtColor(imageD, cv2.COLOR_BGR2GRAY)
 
@@ -71,12 +74,14 @@ def process(ref, img):
     diff = (diff * 255).astype("uint8")
 
     thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    thresh += subimg
+
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
     for c in cnts:
         (x, y, w, h) = cv2.boundingRect(c)
-        if (w > 100 and h > 100) and (x > 300 and x < 900 and y > 200 and y < 520) :
+        if (w > 200 and h > 200) :
              if DEBUG : print("Found object : x = {0}, y = {1}, w = {2}, h = {3}".format(x, y, w, h))
              cv2.imwrite(join("/tmp/","extract.jpg"), imageD[y:y+h, x:x+w])
 
